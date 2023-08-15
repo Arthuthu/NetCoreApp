6337ce62c2349f34e76f41cbfb4000e8e86eea40
Arthuthu <arthurgeromello@hotmail.com>
2023-08-15 12:28:35 -0300

    Add user service

--- Application/Services/Class/UserService.cs
+++ Application/Services/Class/UserService.cs
@@ -1,21 +1,44 @@
-﻿using Domain.Models;
+﻿using Application.Services.Interface;
+using Domain.Models;
 using Domain.Repositories.Interface;
 
 namespace Application.Services.Class;
 
-public sealed class UserService
+public sealed class UserService : IUserService
 {
-	private readonly IUserRepository _userRepository;
+    private readonly IUserRepository _userRepository;
 
-	public UserService(IUserRepository userRepository)
-	{
-		_userRepository = userRepository;
-	}
+    public UserService(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
 
-	public async Task<List<User>?> Get(CancellationToken cancellationToken)
-	{
-		List<User>? users = await _userRepository.Get(cancellationToken);
+    public async Task<List<User>?> Get(CancellationToken cancellationToken)
+    {
+        List<User>? users = await _userRepository.Get(cancellationToken);
 
-		return users is not null ? users : null;
-	}
+        return users is not null ? users : null;
+    }
+
+    public async Task<User?> Get(Guid id, CancellationToken cancellationToken)
+    {
+        User? user = await _userRepository.Get(id, cancellationToken);
+
+        return user is not null ? user : null;
+    }
+
+    public async Task Create(User user, CancellationToken cancellationToken)
+    {
+        await _userRepository.Create(user, cancellationToken);
+    }
+
+    public async Task Update(User user, CancellationToken cancellationToken)
+    {
+        await _userRepository.Update(user, cancellationToken);
+    }
+
+    public async Task Delete(User user, CancellationToken cancellationToken)
+    {
+        await _userRepository.Delete(user, cancellationToken);
+    }
 }
---
+++ Application/Services/Interface/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Models;
+
+namespace Application.Services.Interface
+{
+    public interface IUserService
+    {
+        Task Create(User user, CancellationToken cancellationToken);
+        Task Delete(User user, CancellationToken cancellationToken);
+        Task<List<User>?> Get(CancellationToken cancellationToken);
+        Task<User?> Get(Guid id, CancellationToken cancellationToken);
+        Task Update(User user, CancellationToken cancellationToken);
+    }
+}
