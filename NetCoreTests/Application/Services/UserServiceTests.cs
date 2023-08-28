9e6010aa670c1b635acf5ff5464028f8cb72aa15
Arthur <58709410+Arthuthu@users.noreply.github.com>
2023-08-28 15:09:11 -0300

    Add tests project

---
+++ NetCoreTests/Application/Services/UserServiceTests.cs
@@ -0,0 +1,12 @@
+﻿using Application.Services.User;
+using AutoFixture;
+using Domain.Repositories.User;
+using NSubstitute;
+
+namespace NetCoreTests.Application.Services;
+
+public class UserServiceTests
+{
+    private readonly UserService _sut;
+    private readonly UserRepository _userRepository = Substitute.For<UserRepository>();
+    private readonly Fixture _fixture = new Fixture();
